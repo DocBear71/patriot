00cfbd6bb989ba36e55381941e72e7ea1bb1cbba
Edward McKeown <edward@doctormckeown.com>
2025-04-11 22:14:18 -0500

    fix URL file structure 4-11-25

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     mode: 'development',
@@ -45,5 +46,13 @@ module.exports = {
             template: './src/index.html',
         }),
         new MiniCssExtractPlugin(),
+
+        new CopyWebpackPlugin({
+            patterns: [
+                {from: './src/images', to: 'images'},
+                {from: './src/css', to: 'css'},
+                {from: './src/js', to: 'js'}
+            ]
+        })
     ],
 };
